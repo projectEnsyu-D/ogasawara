bfa0d403e05b0c1bf830d7ebd62ff217e2614439
ogasawara-k23024 <ourinhu269@gmail.com>
2024-05-28 00:14:07 +0900

    余計なコメントの削除

--- bus/js/bus-js/jsonParse.js
+++ bus/js/bus-js/jsonParse.js
@@ -1,39 +1,50 @@
 //時刻のDOM操作
-function onDisplay(hour1, hour2, hour3, minutes1, minutes2, minutes3, isFirst,
-  first, second, third, firstTo, secondTo, thirdTo
+function onDisplay(
+  hour1,
+  hour2,
+  hour3,
+  minutes1,
+  minutes2,
+  minutes3,
+  isFirst,
+  first,
+  second,
+  third,
+  firstTo,
+  secondTo,
+  thirdTo
 ) {
-  //First
-  if(isFirst){
+  //一番近いダイヤ
+  if (isFirst) {
     document.getElementById(firstTo).innerText =
-      "[始発] 0" + hour3 + ":" + ("00" + minutes1).slice(-2);
-  }else if(minutes1 == -1){
+      "[始発] 0" + hour1 + ":" + ("00" + minutes1).slice(-2);
+  } else if (minutes1 == -1 || minutes1 == undefined) {
     document.getElementById(first).style.display = "none";
     document.getElementById(firstTo).innerText = "本日の運行は終了しました";
-  }else{
+  } else {
     document.getElementById(firstTo).innerText =
       hour1 + ":" + ("00" + minutes1).slice(-2);
   }
 
-  //second
-  if(minutes2 == -1){
+  //2番目に近いダイヤ
+  if (minutes2 == -1 || minutes2 == undefined) {
     document.getElementById(second).style.display = "none";
     document.getElementById(secondTo).style.display = "none";
-  }else{
+  } else {
     document.getElementById(secondTo).innerText =
       hour2 + ":" + ("00" + minutes2).slice(-2);
   }
 
-  //third
-  if(minutes2 == -1) {
+  //3番目に近いダイヤ
+  if (minutes3 == -1 || minutes3 == undefined) {
     document.getElementById(third).style.display = "none";
     document.getElementById(thirdTo).style.display = "none";
-  }else{
+  } else {
     document.getElementById(thirdTo).innerText =
       hour3 + ":" + ("00" + minutes3).slice(-2);
   }
 }
 
-//main
 async function main() {
   //バスダイヤの取得
   const firstBusInfoUrl = "https://bus-api.bigbell.dev/api/v1/nextbus?offset=0";
@@ -50,32 +61,24 @@ async function main() {
     response.json()
   );
 
-  //情報の出力用
+  //apiの取得結果の表示
   console.info(firstBusInfo);
   console.info(secondBusInfo);
   console.info(thirdBusInfo);
   console.log("================================");
 
   //バスの運行がない日の場合
-  //HTMLに出力するためのDOM操作
-  //やっぱりDOM操作はonDisplay関数にまとめる
-  //onDisplayは複数回実行するからまとめられない
   if (firstBusInfo["schedule"] == "") {
-    document.getElementById("daiya").innerText 
-      = "本日バスの運行はありません";
+    document.getElementById("daiya").innerText = "本日バスの運行はありません";
     document.getElementById("subtitle").style.display = "none";
     document.getElementById("timeTable").style.display = "none";
     return;
   }
 
   //ダイヤの表示
-  //これもonDisplay関数にまとめた方が良いかも
-  //onDisplayは複数回実行するからまとめられない
-  //セキュリティの観点から文字だけならinnerHTMLよりinnerTextの方がいい
-  document.getElementById("daiya").innerText
-    = "今日は" + firstBusInfo["schedule"] + "ダイヤです";
+  document.getElementById("daiya").innerText =
+    "今日は" + firstBusInfo["schedule"] + "ダイヤです";
 
-  //onDisplay
   //愛工大行き
   onDisplay(
     firstBusInfo["nextHourToAIT"],
@@ -111,7 +114,6 @@ async function main() {
   );
 }
 
-//ここから実際に動く順番
 main();
 
 //タイマー処理
